968ae1f45b1fd9d9f7d798df7bec225f09983ddc
hyunsu9057 <creasia57@gmail.com>
2026-02-11 16:30:24 +0900

    아군 캐릭터 공격범위 생성중

--- Assets/Scripts/Components/AttackControllerComponent.cs
+++ Assets/Scripts/Components/AttackControllerComponent.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using R3;
+
 
 public class AttackControllerComponent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+
+    readonly Subject<Unit>  attackStartSubject = new Subject<Unit>();
+    readonly Subject<Unit> attackEndSubject = new Subject<Unit>();
+    public Observable<Unit> OnAttackStart => attackStartSubject;
+    public Observable<Uint> OnAttackEnd => attackEndSUbject;
+    
+
+    bool isAttacking = false;
+
+    private void Awake() {
+        // 메모리 누수를 위해 소스 추가
+        attackStartSubject.AddTo(this);
+        attackEndSubject.AddTo(this);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void StartAttack() {
+        if(!isAttacking) {
+            Debug.Log("공격시작");
+            isAttacking = true;
+            attackStartSubject.OnNext(Unit.Default);
+        }
+    }
+
+    public void EndAttack() {
+        if(isAttacking){
+            Debug.Log("공격중단");
+            isAttacking = false;
+            attackEndSubject.OnNext(Unit.Default); 
+        }
     }
 }
