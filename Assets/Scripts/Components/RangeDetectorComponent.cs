d0238fc6458bd09c9f44664dfe9db9ea1342d04a
hyunsu9057 <creasia57@gmail.com>
2026-02-18 01:21:54 +0900

    공격범위 컴포넌트 추가

---
+++ Assets/Scripts/Components/RangeDetectorComponent.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using R3;
+
+public class RangeDetectorComponent : MonoBehaviour
+{
+
+    readonly Subject<IDamageable> targetEnteredSubject = new Subject<IDamageable>();
+    readonly Subject<IDamageable> targetExitedSubject = new Subject<IDamageable>();
+    public Subject<IDamageable> OnTargetEnterd => targetEnteredSubject;
+    public Subject<IDamageable> OnTargetExited => targetExitedSubject;
+    
+    [SerializeField] string targetTag = "Enemy";
+
+
+    void Awake() {
+        targetEnteredSubject.AddTo(this);
+        targetExitedSubject.AddTo(this);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag(targetTag)) {
+            var damageable = other.GetComponent<IDamageable>();
+            if(damageable != null) {
+                Debug.Log("공격범위에 들어왔습니다 ");
+                targetEnteredSubject.OnNext(damageable);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag(targetTag)) {
+            var damageable = other.GetComponent<IDamageable>();
+            if(damageable != null) {
+                Debug.Log("공격범위에서 나갔습니다 ");
+                targetExitedSubject.OnNext(damageable);
+            }
+        }
+    }
+}
